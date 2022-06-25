a491e31f5b95cd49474a9f36e7a37e71fa86c8e0
ammy <74745072+Ammy198488@users.noreply.github.com>
2022-06-25 20:42:21 +1300

    Add files via upload

---
+++ javascript 1/index.js
@@ -0,0 +1,9 @@
+let Name = "Amarachukwu";
+let height = 5.8;
+let country = "Nigeria";
+
+let general = Name + " " + height + " " + country;
+// window.alert(general)
+
+
+document.getElementById("demo").innerHTML = general;
